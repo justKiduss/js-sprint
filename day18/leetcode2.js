1840026bfaba80b8b2feb73f65c11ee5426d3e07
justkiduss <kidusteshome036@gmail.com>
2025-11-29 22:45:38 +0300

    doing day18 leetcode exercise

--- day18/leetcode2.js
+++ day18/leetcode2.js
@@ -1,18 +1,16 @@
 var maxProfit = function(prices) {
-    let profit=0;
-    let c=1;
-    for(let i=2;i<prices.length;i++){
-        let maxProfit;
-        if(profit>=maxProfit){
-            maxProfit=profit;
-        }
-        if(prices[i]>prices[i-1]){
-            profit=(prices[i]-prices[i-c])
-            console.log(maxProfit)
-        }
-        c++;
+    let min=Infinity;
+    let maxProfit=0;
+    for(let price of prices){
+       if(min>price){
+        min=price;
+       }
+       let profit=price-min;
+       if(profit>maxProfit){
+        maxProfit=profit;
+       }
     }
     return maxProfit;
 };
-const max=maxProfit([7,1,5,3,6,4]);
-console.log(max)
+const maxi=maxProfit([1,2]);
+console.log(maxi)

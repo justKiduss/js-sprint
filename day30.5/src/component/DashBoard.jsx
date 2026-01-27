eb7353d5916de9e55e8cf84f1786d291c88d8307
justkiduss <kidusteshome036@gmail.com>
2026-01-27 20:35:30 +0300

    adding readme ile to the day 30.5

--- day30.5/src/component/DashBoard.jsx
+++ day30.5/src/component/DashBoard.jsx
@@ -1,26 +1,9 @@
 import useMovie from "../hook/useMovie";
-// import MovieList from "./MovieList";
-
+import MovieList from "./MovieList";
 export default function DashBoard({mode,query}){
         const {status,data,error}=useMovie(mode,query)
         if(status === "idle") return null;
         if(status === "loading") return <p>Loading ...</p>;
         if(status === "error") return <p>{error}</p>
-    return(
-        <>  
-        {/* {!query?
-         loading?<p style={{display:"flex",alignItems:"center",justifyContent:"center"}}>" Loading..."</p>: */}
-            <div>
-               {data.map((data)=>(
-                <div>
-                    <img src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`} alt={data.original_title}/>
-                    <p>{data.original_title}</p>
-                </div>
-               ))}
-            </div>
-        {/* // :
-        // <MovieList mode={mode} query={query}/>
-        // } */}
-        </>
-    )
+    return <MovieList data={data}/>
 }
--- day30.5/src/component/MovieList.jsx
+++ day30.5/src/component/MovieList.jsx
@@ -1,19 +1,13 @@
-import useMovie from "../hook/useMovie"
-
-export default function MovieList({mode,query}){
-    const {data,loading,error}=useMovie(mode,query)
+export default function MovieList({data}){
     return(
         <>   
             <div>
-                {error&& console.log("error while fetching")}
-                {loading?"Loading ...":
-                    data.map((data)=>(
+                    {data.map((data)=>(
                         <div key={data.id}>
                             <img src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`} alt={data.original_title}/>
                             <p>{data.original_title}</p>  
                         </div>
-                    ))
-                }
+                    ))}
             </div>
         </>
     )

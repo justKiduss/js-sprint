060066eb4534dc7de8a338e4ca78acb1590f9288
justkiduss <kidusteshome036@gmail.com>
2026-02-01 22:06:10 +0300

    day34 not finished

--- day34/src/hook/useMovie.js
+++ day34/src/hook/useMovie.js
@@ -2,32 +2,32 @@ import { useEffect, useReducer } from "react";
 import MovieReducer from "../reducers/MovieReducer";
 import { Movies, searchMovie } from "../service/MovieService";
 
-export default async function useMovie(mode,query){
+export default function useMovie(mode,query){
 
     const [state,dispatch]=useReducer(MovieReducer,{status:"idle",data:[],error:null});
     useEffect(()=>{
         let ignore=false;
-        dispatch({status:"LOADING"})
+        dispatch({type:"LOADING"})
         const fetchData=async ()=>{
-            try{
-                const res=query?await searchMovie(query):await Movies()
-                    if(!ignore){
-                        dispatch({
-                            type:"SUCCESS",
-                            payload:res.results
-            })
+        try{
+            const res=query?await searchMovie(query):await Movies()
+            if(!ignore){
+                dispatch({
+                type:"SUCCESS",
+                payload:res.results
+        })
         }}catch(err){
              if(!ignore){
                 dispatch({
                     type:"FAILURE",
                     payload:err.message
                 })}
-            }
+            }}
             fetchData();
+            
             return ()=>{
                 ignore=true;
             }
-        } 
     },[mode,query])
     console.log(state);
     return state
--- day34/src/service/MovieService.js
+++ day34/src/service/MovieService.js
@@ -2,7 +2,7 @@ const APIKEY="9ba860f27b8af2a4d997f80a66b063b5";
 
 export async function Movies(){
     try{
-        const res=fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`)
+        const res=await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`)
         return await res.json();
     }catch(err){
         throw new Error("error while fetching movie data from the api");
@@ -11,7 +11,7 @@ export async function Movies(){
 
 export async function searchMovie(query){
     try{
-        const res=fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}`)
+        const res=await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}`)
         return await res.json();
     }catch(err){
         throw new Error ("error while fetching movie data from the api")
